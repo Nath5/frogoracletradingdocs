b079a6dbc08ff5d9ec0a0594df766481d495f3cf
Nathan Case <Nath5@users.noreply.github.com>
2024-10-18 16:31:04 -0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://docs.frogoracletrading.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/frogoracledocumentation_test/',
+  baseUrl: '/frogoracledocumentation_test/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
